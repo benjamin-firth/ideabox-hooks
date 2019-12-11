730c13322810a99add3cdd7c7fd3e1e741683fe9
Ben Firth <bfirth93@gmail.com>
2019-12-11 10:40:23 -0700

    Create snapshot test for app

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('App', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<App />)
+  })
+
+  describe('App Snapshots', () => {
+    it('Should match the snapshot', () => {
+      expect(wrapper).toMatchSnapshot();
+    })
+  })
+
+})

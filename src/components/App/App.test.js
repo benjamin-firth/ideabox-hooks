3c4254247fbcb54573f950c969b719c933a52bc8
Ben Firth <bfirth93@gmail.com>
2019-12-11 12:06:08 -0700

    Add final commit and update snapshots

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -15,4 +15,50 @@ describe('App', () => {
     })
   })
 
+  // describe('State changes in App', () => {
+  
+
+  //   // I tried to mock out fetch so that I could show the second change in state, but I wasn't able to in the time constraints.  Perhaps next time I change state before I make the POST request?Not sure on the best practice there.
+
+
+  //   it('Should change state when addNewReservation is fired', () => {
+
+  //     const initialState = {
+  //       ogReservations: [
+  //         {
+  //           name: '',
+  //           date: '',
+  //           time: '',
+  //           number: 0
+  //         }
+  //       ]
+  //     }
+
+  //     const expected = {
+  //       ogReservations: [
+  //         {
+  //           name: 'John Adams',
+  //           date: '',
+  //           time: '',
+  //           number: 0
+  //         }
+  //       ]
+  //     }
+
+  //     window.fetch = jest.fn().mockImplementation(() => {
+  //       return Promise.resolve({
+  //         ok: true,
+  //         json: () => {
+  //           return Promise.resolve()
+  //         }
+  //       })
+  //     })
+
+  //     wrapper.setState(initialState);
+  //     wrapper.instance().addNewReservation("John Adams", '', '', 0);
+  //     expect(wrapper.state()).toEqual(expected);
+  //   })
+
+  // })
+
 })

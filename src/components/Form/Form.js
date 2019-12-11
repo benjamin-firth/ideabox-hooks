63d1201d24f73236e4bc8bb6c2d9c5b99973545b
Ben Firth <bfirth93@gmail.com>
2019-12-11 10:17:19 -0700

    Add handlechange that updates state within form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -4,16 +4,26 @@ import './Form.css';
 class Form extends Component {
   constructor() {
     super()
-    this.state = {}
+    this.state = {
+      name: '',
+      date: '',
+      time: '',
+      number: 0
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value});
+    console.log(this.state);
   }
 
   render() {
     return(
       <section className="enter-form">
-        <input placeHolder="Name" />
-        <input placeHolder="Date" />
-        <input placeHolder="Time" />
-        <input placeHolder="Number of Guests" />
+        <input name="name" value={this.state.name} onChange={(e) => this.handleChange(e)} placeHolder="Name" />
+        <input name="date" value={this.state.date} onChange={(e) => this.handleChange(e)} placeHolder="Date" />
+        <input name="time" value={this.state.time} onChange={(e) => this.handleChange(e)} placeHolder="Time" />
+        <input name="number" value={this.state.number} onChange={(e) => this.handleChange(e)} placeHolder="Number of Guests" />
         <button>Make Reservation</button>
       </section>
     )

4c7893a7445a05dadb92dd3476a3782cbcdbe74e
Ben Firth <bfirth93@gmail.com>
2019-12-11 10:29:07 -0700

    Add post on button click

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import './Form.css';
 
 class Form extends Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
     this.state = {
       name: '',
       date: '',
@@ -20,11 +20,25 @@ class Form extends Component {
   render() {
     return(
       <section className="enter-form">
-        <input name="name" value={this.state.name} onChange={(e) => this.handleChange(e)} placeHolder="Name" />
-        <input name="date" value={this.state.date} onChange={(e) => this.handleChange(e)} placeHolder="Date" />
-        <input name="time" value={this.state.time} onChange={(e) => this.handleChange(e)} placeHolder="Time" />
-        <input name="number" value={this.state.number} onChange={(e) => this.handleChange(e)} placeHolder="Number of Guests" />
-        <button>Make Reservation</button>
+        <input 
+          name="name" 
+          value={this.state.name} 
+          onChange={(e) => this.handleChange(e)} placeHolder="Name" />
+        <input 
+          name="date" 
+          value={this.state.date} 
+          onChange={(e) => this.handleChange(e)} placeHolder="Date" />
+        <input 
+          name="time" 
+          value={this.state.time} 
+          onChange={(e) => this.handleChange(e)} placeHolder="Time" />
+        <input 
+          name="number" 
+          value={this.state.number} 
+          onChange={(e) => this.handleChange(e)} placeHolder="Number of Guests" />
+        <button 
+          type="button" 
+          onClick={() => this.props.addNewReservation(this.state.name, this.state.date, this.state.time, this.state.number)}>Make Reservation</button>
       </section>
     )
   }
